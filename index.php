0f66a09a11208088743b3ec55b6ea44a475a4145
Cláudio Vasconcellos <claudiofreitasvasconcellos@gmail.com>
2026-03-11 10:54:56 -0300

    Realizei a feature de criar tarefas

--- index.php
+++ index.php
@@ -1,18 +1,54 @@
 <?php
 
-echo "Olá mundo!";
+    $json = file_get_contents("tarefas.json");
+
+    $tarefas = json_decode($json, true);
+
+
+    foreach($tarefas as $tarefa){
+
+        $classe = "";
+
+        if($tarefa["concluida"]){
+        $classe = "concluida";
+        }
+
+        echo "<div class='card $classe'>";
+
+        echo "<h3>".$tarefa["nome"]."</h3>";
+
+        if($tarefa["concluida"]){
+        echo "<p>✅ Concluída</p>";
+        }else{
+        echo "<p>⏳ Pendente</p>";
+        }
+
+        echo "</div>";
+
+        }
+
+
+
+
 
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 <body>
-    
+
+
+    <a href="criar_tarefa.php">
+        <button>Criar tarefa</button>
+    </a>
+
+
+
 </body>
 </html>
 
